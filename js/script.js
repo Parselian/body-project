e47532af2be3e2082311e5df54218710ac09fe2b
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-12-02 17:30:37 +0300

    добавил управление показом всплывающих окон и меню выбора клуба

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use_strict';
 
-  
+  //управление всплывающими блоками
+  const toggleModals = () => {
+    const clubsList = document.querySelector('.clubs-list > ul'),
+          freeVisitForm = document.getElementById('free_visit_form'),
+          callbackForm = document.getElementById('callback_form'),
+          giftModal = document.getElementById('gift');
+
+    document.addEventListener('click', (e) => {
+      const target = e.target;
+
+      switch(target) {
+        case target.closest('.clubs-list__title'):
+          clubsList.classList.toggle('show');
+          break;
+        case target.closest('.open-popup'):
+          freeVisitForm.classList.add('show');
+          break;
+        case target.closest('#header-callback__btn'):
+          callbackForm.classList.add('show');
+          break;
+        case target.closest('.fixed-gift__img'):
+          target.closest('.fixed-gift').remove();
+          giftModal.classList.add('show');
+          break;
+        case target.closest('.close-btn, .close_icon, .overlay'):
+          freeVisitForm.classList.remove('show');
+          giftModal.classList.remove('show');
+          callbackForm.classList.remove('show');
+          break;
+      }
+    });
+  };
+  toggleModals();
 });
